2741d12276b5144892e7ae551da7d86d71246f80
zxcursedsocute <130180185+zxcursedsocute@users.noreply.github.com>
2025-04-28 00:09:47 +0300

    Delete buzut directory

--- buzut/пупсик/db/db_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbSetup="mysql:host=localhost;dbname=3319v;charset=utf8";
-$dbUser="root";
-$dbPass="root";
-$dbErrMode=[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-$conn = new PDO($dbSetup, $dbUser, $dbPass, $dbErrMode);
-?>
